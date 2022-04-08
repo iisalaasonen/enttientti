c2fb7217d3c3287a856489a4bf219f01d9fcde32
Iisa Laasonen <iisa.laasonen@gmail.com>
2022-04-09 00:29:10 +0300

    spacing

--- imports/ui/pages/common/ContainerPage.jsx
+++ imports/ui/pages/common/ContainerPage.jsx
@@ -4,17 +4,15 @@ import useTheme from "@mui/material/styles/useTheme";
 
 const ContainerPage = ({ children }) => {
   const theme = useTheme();
-  return (
-    <Container
-      sx={{
-        backgroundColor: theme.palette.background.default,
-        height: "100vh",
-        margin: theme.spacing(4, "auto"),
-      }}
-    >
-      {children}
-    </Container>
-  );
+
+  const containerStyle = {
+    backgroundColor: theme.palette.background.default,
+    height: "100vh",
+    width: "80vw",
+    margin: theme.spacing(4, "auto"),
+  };
+
+  return <Container sx={containerStyle}>{children}</Container>;
 };
 
 export default ContainerPage;
